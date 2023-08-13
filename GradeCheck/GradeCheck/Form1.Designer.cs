cc1bc8e8940dd7bb1895226d6491df8ebbedbdce
Lucana <k.ark.scarlet72@gmail.com>
2023-08-13 15:45:34 +0900

    [Finish]タブオーダーを設定し、一度実行して確認。

--- GradeCheck/GradeCheck/Form1.Designer.cs
+++ GradeCheck/GradeCheck/Form1.Designer.cs
@@ -115,7 +115,7 @@
             this.label7.Location = new System.Drawing.Point(38, 115);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(29, 12);
-            this.label7.TabIndex = 6;
+            this.label7.TabIndex = 11;
             this.label7.Text = "物理";
             // 
             // label8
@@ -124,7 +124,7 @@
             this.label8.Location = new System.Drawing.Point(38, 167);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(29, 12);
-            this.label8.TabIndex = 7;
+            this.label8.TabIndex = 17;
             this.label8.Text = "英語";
             // 
             // textBoxAttendanceM
@@ -132,7 +132,7 @@
             this.textBoxAttendanceM.Location = new System.Drawing.Point(113, 61);
             this.textBoxAttendanceM.Name = "textBoxAttendanceM";
             this.textBoxAttendanceM.Size = new System.Drawing.Size(100, 19);
-            this.textBoxAttendanceM.TabIndex = 8;
+            this.textBoxAttendanceM.TabIndex = 6;
             this.textBoxAttendanceM.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBoxAttendanceP
@@ -140,7 +140,7 @@
             this.textBoxAttendanceP.Location = new System.Drawing.Point(113, 112);
             this.textBoxAttendanceP.Name = "textBoxAttendanceP";
             this.textBoxAttendanceP.Size = new System.Drawing.Size(100, 19);
-            this.textBoxAttendanceP.TabIndex = 9;
+            this.textBoxAttendanceP.TabIndex = 12;
             this.textBoxAttendanceP.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBoxAttendanceE
@@ -148,7 +148,7 @@
             this.textBoxAttendanceE.Location = new System.Drawing.Point(113, 167);
             this.textBoxAttendanceE.Name = "textBoxAttendanceE";
             this.textBoxAttendanceE.Size = new System.Drawing.Size(100, 19);
-            this.textBoxAttendanceE.TabIndex = 10;
+            this.textBoxAttendanceE.TabIndex = 18;
             this.textBoxAttendanceE.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBoxScoreM
@@ -156,7 +156,7 @@
             this.textBoxScoreM.Location = new System.Drawing.Point(302, 61);
             this.textBoxScoreM.Name = "textBoxScoreM";
             this.textBoxScoreM.Size = new System.Drawing.Size(100, 19);
-            this.textBoxScoreM.TabIndex = 11;
+            this.textBoxScoreM.TabIndex = 8;
             this.textBoxScoreM.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBoxScoreP
@@ -164,7 +164,7 @@
             this.textBoxScoreP.Location = new System.Drawing.Point(302, 112);
             this.textBoxScoreP.Name = "textBoxScoreP";
             this.textBoxScoreP.Size = new System.Drawing.Size(100, 19);
-            this.textBoxScoreP.TabIndex = 12;
+            this.textBoxScoreP.TabIndex = 14;
             this.textBoxScoreP.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBoxScoreE
@@ -172,7 +172,7 @@
             this.textBoxScoreE.Location = new System.Drawing.Point(302, 167);
             this.textBoxScoreE.Name = "textBoxScoreE";
             this.textBoxScoreE.Size = new System.Drawing.Size(100, 19);
-            this.textBoxScoreE.TabIndex = 13;
+            this.textBoxScoreE.TabIndex = 20;
             this.textBoxScoreE.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // label9
@@ -181,7 +181,7 @@
             this.label9.Location = new System.Drawing.Point(219, 68);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(11, 12);
-            this.label9.TabIndex = 14;
+            this.label9.TabIndex = 7;
             this.label9.Text = "%";
             // 
             // label10
@@ -190,7 +190,7 @@
             this.label10.Location = new System.Drawing.Point(219, 119);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(11, 12);
-            this.label10.TabIndex = 15;
+            this.label10.TabIndex = 13;
             this.label10.Text = "%";
             // 
             // label11
@@ -199,7 +199,7 @@
             this.label11.Location = new System.Drawing.Point(219, 174);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(11, 12);
-            this.label11.TabIndex = 16;
+            this.label11.TabIndex = 19;
             this.label11.Text = "%";
             // 
             // labelResultM
@@ -208,7 +208,7 @@
             this.labelResultM.Location = new System.Drawing.Point(444, 68);
             this.labelResultM.Name = "labelResultM";
             this.labelResultM.Size = new System.Drawing.Size(41, 12);
-            this.labelResultM.TabIndex = 17;
+            this.labelResultM.TabIndex = 9;
             this.labelResultM.Text = "label12";
             // 
             // labelResultP
@@ -217,7 +217,7 @@
             this.labelResultP.Location = new System.Drawing.Point(444, 119);
             this.labelResultP.Name = "labelResultP";
             this.labelResultP.Size = new System.Drawing.Size(41, 12);
-            this.labelResultP.TabIndex = 18;
+            this.labelResultP.TabIndex = 15;
             this.labelResultP.Text = "label13";
             // 
             // labelResultE
@@ -226,7 +226,7 @@
             this.labelResultE.Location = new System.Drawing.Point(444, 174);
             this.labelResultE.Name = "labelResultE";
             this.labelResultE.Size = new System.Drawing.Size(41, 12);
-            this.labelResultE.TabIndex = 19;
+            this.labelResultE.TabIndex = 21;
             this.labelResultE.Text = "label14";
             // 
             // buttonJudge
@@ -234,7 +234,7 @@
             this.buttonJudge.Location = new System.Drawing.Point(269, 237);
             this.buttonJudge.Name = "buttonJudge";
             this.buttonJudge.Size = new System.Drawing.Size(75, 23);
-            this.buttonJudge.TabIndex = 20;
+            this.buttonJudge.TabIndex = 23;
             this.buttonJudge.Text = "判定";
             this.buttonJudge.UseVisualStyleBackColor = true;
             // 
@@ -243,7 +243,7 @@
             this.buttonReset.Location = new System.Drawing.Point(375, 237);
             this.buttonReset.Name = "buttonReset";
             this.buttonReset.Size = new System.Drawing.Size(75, 23);
-            this.buttonReset.TabIndex = 21;
+            this.buttonReset.TabIndex = 24;
             this.buttonReset.Text = "リセット";
             this.buttonReset.UseVisualStyleBackColor = true;
             // 
@@ -253,7 +253,7 @@
             this.labelCompAvgM.Location = new System.Drawing.Point(539, 68);
             this.labelCompAvgM.Name = "labelCompAvgM";
             this.labelCompAvgM.Size = new System.Drawing.Size(41, 12);
-            this.labelCompAvgM.TabIndex = 22;
+            this.labelCompAvgM.TabIndex = 10;
             this.labelCompAvgM.Text = "label15";
             // 
             // labelCompAvgP
@@ -262,7 +262,7 @@
             this.labelCompAvgP.Location = new System.Drawing.Point(539, 119);
             this.labelCompAvgP.Name = "labelCompAvgP";
             this.labelCompAvgP.Size = new System.Drawing.Size(41, 12);
-            this.labelCompAvgP.TabIndex = 23;
+            this.labelCompAvgP.TabIndex = 16;
             this.labelCompAvgP.Text = "label16";
             // 
             // labelCompAvgE
@@ -271,7 +271,7 @@
             this.labelCompAvgE.Location = new System.Drawing.Point(539, 174);
             this.labelCompAvgE.Name = "labelCompAvgE";
             this.labelCompAvgE.Size = new System.Drawing.Size(41, 12);
-            this.labelCompAvgE.TabIndex = 24;
+            this.labelCompAvgE.TabIndex = 22;
             this.labelCompAvgE.Text = "label17";
             // 
             // Form1
